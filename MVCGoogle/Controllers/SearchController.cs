b6d2a5d21b9adbc0ee48cfcac6bc4fdbced6a45c
Alison Bell <abell@medtouch.com>
2016-06-13 11:36:01 -0400

    Search box that sends query to data source

---
+++ MVCGoogle/Controllers/SearchController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCGoogle.Controllers
+{
+    using Sitecore.Data;
+    using Sitecore.Links;
+    using Sitecore.Mvc.Configuration;
+    using Sitecore.Mvc.Presentation;
+
+    public class SearchController : Controller
+    {
+        //
+        // GET: /Search/
+
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public ActionResult Search(String query, String dataSource)
+        {
+            var options = new UrlOptions
+            {
+                AddAspxExtension = false,
+                LanguageEmbedding = LanguageEmbedding.Never
+            };
+
+            var pathInfo = (LinkManager.GetItemUrl(Sitecore.Context.Database.GetItem(new ID(dataSource)), options));
+
+            return RedirectToRoute(MvcSettings.SitecoreRouteName, new { pathInfo = pathInfo.TrimStart(new char[] { '/' }), q = query});
+        }
+    }
+}
